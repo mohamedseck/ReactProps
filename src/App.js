02a90bfcd8bf5d6d42a9f84bfcb62164d3660d3a
Mohamedseck <ametnaar@me.com>
2022-02-11 16:35:09 +0000

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import './App.css';
+import React from 'react'
 import Profile from './Profile/Profile';
+import './App.css';
+class App extends React.Component {
+  // =============================Profile & bio=============================
+  constructor(props){
+    super(props);
+    this.state={
+      Person :{ 
+                fullName:   'Mohamed Seck',
+                bio:        'Full stack web development @ GoMyCode,  I am learning full stack web development and enjoy computer programming',
+                imgSrc:     '/assets/images.png',
+                professio:  'Full Stack Software Engineering'
+              },
+      show:false ,
+      count: 0
+              };
+  }
+  
+  // =============================button toggle function=============================
+  toggle =() =>{this.setState({show:!this.state.show});}
 
+  // ========================increment Counter function========================
+  increment = () => {this.state.show ? this.setState({ count: this.state.count + 1 }) : this.setState({ count: 0 });};
 
-function App() {
-  const user={fullName:'Mohamed Seck'  ,
-              bio:'I am a Full stack software development, I have experience in Finance and Program Manager. ',
-              profession:'Senior Program Manager'};
-  const handleName=(user)=>{alert(`Your Name's : ${user.fullName}`)};
+  // ========================componentDidMount lifecyle========================
+  componentDidMount = () => {setInterval(this.increment, 1000);}
 
-
-  return (
-          <div className='App'>
-            <h1>Profile User</h1>
-            <Profile user={user} handleName={handleName}>
-            <img src='/assets/images.png' alt='Images' className='avatar'/>
-              
-            </Profile>
-          </div>
+  render(){
+    return (
+    <div className="App">
+      {/* counter */}
+      <h4 style={{ color: '#457b9d' }}>
+        {this.state.count} seconds .
+      </h4>
+      {/* show button */}
+      <button className='btnShow' onClick={this.toggle}>Show Profile</button>
+      {this.state.show && <Profile Person={this.state.Person}/>}
+    </div>
   );
+  }
 }
-
-
 export default App;
